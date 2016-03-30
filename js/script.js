9bbfd6abcbb110d845ee0aaa0c4ef1767ee6b38e
Katya <katyaskvo@gmail.com>
2016-03-29 22:51:13 -0700

    slot machine main functionality

--- js/script.js
+++ js/script.js
@@ -1,127 +1,70 @@
-/*
-var reel1 = ["coffee-maker", "espresso-machine", "teapot"];
-var reel2 = ["coffee-filter", "espresso-tamper", "tea-strainer"];
-var reel3 = ["coffee-beans", "ground-coffee", "tea-leaf"];
-
-function go(){
-	moveSlots($("#reel1 .wrapper"));
-	moveSlots($("#reel2 .wrapper"));
-	moveSlots($("#reel3 .wrapper"));
-}
-
-function moveSlots(reel){
-		var time = 6500;
-		time += Math.round(Math.random()*1000);
-	reel.stop(true,true);
-
-		var marginTop = parseInt(reel.css("margin-top"), 10)
-		
-		marginTop -= (7 * 100)
-		
-	reel.animate(
-		{"margin-top":marginTop+"px"},
-		{'duration' : time, 'easing' : "easeOutElastic"});
-
-}
-*/
-
-
 var reels = [
 				['coffee-maker',   'teapot',       'espresso-machine'],
+				['coffee-maker',   'teapot',       'espresso-machine'],
+				['coffee-maker',   'teapot',       'espresso-machine']
+				/*
+
 				['coffee-filter',  'tea-strainer', 'espresso-tamper'],
 				['coffee-grounds', 'loose-tea',    'ground-espresso-beans']
+*/
 			];
-console.log(reels);
 var $reels;
-console.log($reels);
 var spinningTime = 2000;
+var reelRotations = [];
+var cssRotationValue = "";
+var positions = [0, 0, 0];
 
 
 
 function init(){
-	$reels = $('.reel').each(function(i, reelEach){
-		
-		var reelItems = "";
+	$('.reel').each(function(i, reelContent){
 		for (j = 0; j < 3; j++) {
-			reelItem = '<div id="' + reels[i][j] + '" class="slot"><img src="img/' + reels[i][j] + '.png" /></div>';
-			reelItems += reelItem;
+			reelContent.innerHTML += '<img src="img/' + reels[i][j] + '.png" />';
 		}
-		reelEach.innerHTML = reelItems;
-		console.log(reelEach);
-		
 	});
 
-	$msg = $('.msg');
+	$('button').click(play);
+}
 
-// 		$('button').click(action);
-	}
-/*
-var caffeineMachine = (function(undefined){
+function play(){
 	
-	var timeMax = 3000, // animation time, ms
-		height = 210,
-		reelSpeeds = [],
-		r = [],
-		reels = [
-			['coffee_maker',   'teapot',       'espresso_machine'],
-			['coffee_filter',  'tea_strainer', 'espresso_tamper'],
-			['coffee_grounds', 'loose_tea',    'ground_espresso_beans']
-		],
-		$reels, $msg,
-		start;
-		console.log(reels);
-
-	function init(){
-		console.log("or here?");
-		$reels = $('.reel').each(function(i, el){
-			el.innerHTML = '<div><p>' + reels[i].join('</p><p>') + '</p></div><div><p>' + reels[i].join('</p><p>') + '</p></div>'
-		});
-
-		$msg = $('.msg');
-
-		$('button').click(action);
+	for (var i = 0; i < 3; ++i) {
+		reelRotations[i] = - (Math.floor(Math.random() * 30) + 20) * 120;	 // random number from 10 to 40 multiplied by 120 degree
+		positions[i] = (reelRotations[i] / 120) % 3  // calculates reel index position
 	}
+		
+	animate();
+	setTimeout(clearRotationDegree, 5000);
 
-	function action(){
-		if (start !== undefined) return;
+}
 
-		for (var i = 0; i < 3; ++i) {
-			speeds[i] = Math.random() + .5;	
-			r[i] = (Math.random() * 3 | 0) * height / 3;
-		}
+function animate(){
+	var cssRotationValue = "";
+	$('.reel').each(function(i, reel){
+		cssRotationValue = "rotateX(" + reelRotations[i] + "deg)";
+		
+        console.log("rot value: " + reelRotations[i]);
+		$(reel).css({
+			"-moz-transform":cssRotationValue,
+	        "-webkit-transform":cssRotationValue,
+	        "transition":"5s ease all"
+        });
+	});
+}
 
-		$msg.html('Spinning...');
-		animate();
-	}
+function clearRotationDegree(){
+	
+	$('.reel').each(function(i, reel){
+        initialCssRotationValue = positions[i] * 120;
+        console.log("init rot value: " + initialCssRotationValue);
+        initialCssRotationValue = "rotateX(" + initialCssRotationValue + "deg)";
 
-	function animate(now){
-		console.log("gets here?");
-		if (!start) start = now;
-		var t = now - start || 0;
-
-		for (var i = 0; i < 3; ++i)
-			$reels[i].scrollTop = (speeds[i] / tMax / 2 * (tMax - t) * (tMax - t) + r[i]) % height | 0;
-
-		if (t < tMax)
-			requestAnimationFrame(animate);
-		else {
-			start = undefined;
-			check();
-		}
-	}
-
-	function check(){
-		$msg.html(
-			r[0] === r[1] && r[1] === r[2] ?
-				'You won! Enjoy your ' + reels[1][ (r[0] / 70 + 1) % 3 | 0 ].split(' ')[0]
-			:
-				'Try again'
-		);
-	}
-
-	return {init: init}
-
-})();
-*/
+        $(reel).css({
+			"-moz-transform":initialCssRotationValue,
+	        "-webkit-transform":initialCssRotationValue,
+	        "transition":"0s ease all"
+        });
+	});
+}
 
 init();
